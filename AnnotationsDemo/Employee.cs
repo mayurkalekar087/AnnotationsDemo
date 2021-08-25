94b1801ec7c5216efa43e773a09090ae1f007f18
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-25 19:30:07 +0530

    Add project files.

---
+++ AnnotationsDemo/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnnotationsDemo
+{
+    public class Employee
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        [Required(ErrorMessage ="Employee {0} is Required")]
+        [StringLength(100, MinimumLength =3, ErrorMessage ="Name Should be minimum 3 char and maximum 100 char")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        /// <value>
+        /// The age.
+        /// </value>
+        [Range(18, 99, ErrorMessage = "Age should be above 18")]
+        public string Age { get; set; }
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>
+        /// The phone number.
+        /// </value>
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ AnnotationsDemo/TestAnnotations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace AnnotationsDemo
+{
+    class TestAnnotations
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Emplyoee Class Validation");
+            Console.WriteLine("-------------------------");
+
+            Employee objEmployee = new Employee();
+            objEmployee.Name = "Mayur Kalekar";
+            objEmployee.Age = "18";
+            objEmployee.PhoneNumber = "8888888888";
+            objEmployee.Email = "mayurKalekar250@gmail.com";
+
+            ValidationContext context = new ValidationContext(objEmployee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(objEmployee, context, results, true);
+
+            if (!valid)
+            {
+                foreach (ValidationResult Totalresult in results)
+                {
+                    Console.WriteLine("Member Name:{0}", Totalresult.MemberNames.First(),Environment.NewLine);
+                    Console.WriteLine("Error Mesage :: {0}{1}", Totalresult.ErrorMessage, Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name: " + objEmployee.Name + "\n" + "Age: " + objEmployee.Age + "\n" + "Phonenumber: " + objEmployee.PhoneNumber + "\n" + "Email: " + objEmployee.Email);
+            }
+            Console.WriteLine("\n Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
